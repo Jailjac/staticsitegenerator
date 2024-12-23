3eea98112a4f733b6e703392f3245ace9128d476
Jailjac <jacobmazac@gmail.com>
2024-12-22 23:34:12 -0800

    Added and tested LeafNode object

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,16 @@ class HTMLNode():
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("Leaf node has no value")
+        html_string = self.value
+        if self.tag:
+            html_string = f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+        return html_string
+    
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_none(self):
@@ -14,6 +14,23 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_multiple(self):
         node1 = HTMLNode(props={"link":"google.com", "notes":"Search Engine"})
         self.assertEqual(node1.props_to_html(), ' link="google.com" notes="Search Engine"')
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode(tag = "p", value= None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(tag = None, value="Tagless little loser", props={"href":"virus.com"})
+        self.assertEqual(node.to_html(), "Tagless little loser")
+
+    def test_leaf_to_html_no_props(self):
+        node = LeafNode(tag="h1", value="Propless in Seattle")
+        self.assertEqual(node.to_html(), "<h1>Propless in Seattle</h1>")
+    
+    def test_leaf_to_html_full(self):
+        node = LeafNode(tag="p", value="I've got the full package", props={"href" : "google.com", "img" : "puppy.png"})
+        self.assertEqual(node.to_html(), '<p href="google.com" img="puppy.png">I\'ve got the full package</p>')
 
 
 if __name__ == "__main__":
